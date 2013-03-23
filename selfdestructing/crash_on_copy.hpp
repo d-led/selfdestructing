c2c223b3430c04e8ac5d70dc797f1019072605fc
d-led <dmlled@yahoo.com>
2013-03-23 16:20:18 +0100

    further refactoring

--- selfdestructing/crash_on_copy.hpp
+++ selfdestructing/crash_on_copy.hpp
@@ -9,14 +9,29 @@ namespace crashes {
 
 	namespace detail {
 
-		////////////////////////////////////////
+		//////////////////////////////////////////////
 		typedef std::shared_ptr<size_t> shared_number;
-		
-		/////////////////////////////////////////
+
+		class simple_count {
+			size_t copy_nr;
+		public:
+
+			simple_count():copy_nr(0){}
+
+			size_t get_copy_nr() const {
+				return copy_nr;
+			}
+
+			void increment() {
+				++copy_nr;
+			}
+		};
+
+		////////////////////////////////////////////
 		template <typename TSharedInt=shared_number>
 		class shared_count {
 			TSharedInt copy_nr;
-		
+
 		public:
 
 			shared_count():copy_nr(new size_t(0)){}
@@ -29,88 +44,56 @@ namespace crashes {
 				++(*copy_nr);
 			}
 		};
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		template <size_t MaxN,typename TFeedback=on_feedback, typename TCounter=shared_count<shared_number>>
+		struct when {
+			class copies {
+				TCounter counter;
+				TFeedback feedback;
+			public:
+
+				copies() { }
+
+				copies(const copies& other):
+					counter(other.counter),
+					feedback(other.feedback)
+				{
+					counter.increment();
+					size_t count=counter.get_copy_nr();
+					if (feedback)
+						feedback(count);
+					if (count>=MaxN)
+						throw std::runtime_error("illegal number of copies");
+				}
+
+				copies& operator=(copies const& other)  {
+					copies tmp(other);
+					swap(other);
+					return *this;
+				}
+
+				void swap(copies const& other) {
+					std::swap(counter,other.counter);
+					std::swap(feedback,other.feedback);
+				}
+
+			public:
+
+				void set_feedback(TFeedback const& f) {
+					feedback=f;
+				}
+
+			};
+			typedef copies copy;
+		};
 	}
 
-	//////////////////////////////////////////////////////////////////////////////////////////
-	template <int MaxN,typename TFeedback=on_feedback, typename TCounter=detail::shared_count<detail::shared_number>>
-	struct on {
-		class copies {
-			TCounter counter;
-			TFeedback feedback;
-		public:
-			
-			copies() { }
+	template <size_t MaxN>
+	struct on : public detail::when<MaxN,on_feedback,detail::shared_count<detail::shared_number>>
+	{};
 
-			copies(const copies& other):
-				counter(other.counter),
-				feedback(other.feedback)
-			{
-				counter.increment();
-				size_t count=counter.get_copy_nr();
-				if (feedback)
-					feedback(count);
-				if (count>=MaxN)
-					throw std::runtime_error("illegal number of copies");
-			}
-
-			copies& operator=(copies const& other)  {
-				copies tmp(other);
-				swap(other);
-				return *this;
-			}
-
-			void swap(copies const& other) {
-				std::swap(counter,other.counter);
-				std::swap(feedback,other.feedback);
-			}
-
-		public:
-
-			void set_feedback(TFeedback const& f) {
-				feedback=f;
-			}
-
-		};
-		typedef copies copy;
-	};
-
-	template <int MaxN,typename TFeedback=on_feedback>
-	struct after {
-		class copies {
-			int copy_nr;
-			TFeedback feedback;
-		public:
-			
-			copies():copy_nr(0) { }
-
-			copies(const copies& other):
-				copy_nr(other.copy_nr+1),
-				feedback(other.feedback)
-			{
-				if (feedback)
-					feedback(copy_nr);
-				if (copy_nr>=MaxN)
-					throw std::runtime_error("illegal copy");
-			}
-
-			copies& operator=(copies const& other)  {
-				copies tmp(other);
-				swap(other);
-				return *this;
-			}
-
-			void swap(copies const& other) {
-				std::swap(copy_nr,other.copy_nr);
-				std::swap(feedback,other.feedback);
-			}
-
-		public:
-
-			void set_feedback(TFeedback const& f) {
-				feedback=f;
-			}
-
-		};
-		typedef copies copy;
-	};
+	template <size_t MaxN>
+	struct after : public detail::when<MaxN,on_feedback,detail::simple_count>
+	{};
 }
